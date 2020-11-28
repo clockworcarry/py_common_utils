51131e9ec32571cced83d40c233b9299963b1a67
Gabriel Helie <ghelie@hotmail.com>
2020-11-28 17:27:35 -0500

    inc version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="py_common_utils_gh",
-    version="1.0.0",
+    version="1.0.1",
     author="Gabriel Helie",
     author_email="ghfin123@gmail.com",
     description="All-purpose python utils",
